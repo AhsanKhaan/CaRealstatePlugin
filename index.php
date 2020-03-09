936eba6c91c20693e319402976e297335018431f
AhsanKhaan <ahsankhan.ubit@gmail.com>
2020-03-09 17:01:12 +0500

    function added to Redirect as wordpress root URL  instead of plugin URL

--- index.php
+++ index.php
@@ -82,6 +82,20 @@ function my_activation() {
 
    
 }
+////////////////////////////////////////////////////////////////
+///  Redirect as wordpress root URL  instead of plugin URL
+////////////////////////////////////////////////////////////////
+add_action( 'init', 'wpse9870_init_external' );
+function wpse9870_init_external()
+{
+    global $wp_rewrite;
+    $plugin_url = plugins_url( 'ListingDetail.php', __FILE__ );
+    $plugin_url = substr( $plugin_url, strlen( home_url() ) + 1 );
+    // The pattern is prefixed with '^'
+    // The substitution is prefixed with the "home root", at least a '/'
+    // This is equivalent to appending it to `non_wp_rules`
+    $wp_rewrite->add_external_rule( 'ListingDetail.php$', $plugin_url );
+}
 function displayListing(){
 ////////////////////////////////////////////////
 ///// Register all style sheets and scripts ///
