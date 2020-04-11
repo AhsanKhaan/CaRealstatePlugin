603a84d9808fc494148fa823f5e2bc7cdd322dce
Ahsankhaan <ahsankhan.ubit@gmail.com>
2020-04-11 19:29:42 +0500

    show cards on front end

--- js/app.js
+++ js/app.js
@@ -14,39 +14,76 @@ $("#search").click(function(){
   function showdata(dataset){
     dataset.property.forEach(data => {
      // alert(data["TotalCount"]);
-      var html=`
-      <div class="col-md-4">
-    <div class="card" >
-        <h1>`+data['TransactionType']+`</h1>
-        <h4>ID#`+data['ID']+`</h4>
-        <a href="http://localhost:80/wordpress/wordpress/wp-content/plugins/crea/ListingDetail.php?ID=`+data['ID']+`" target="_blank"><img class="card-img-top" src="`+data['Photo']+`" alt="Card image"></a>
-        <div class="card-body">
-          <h3 style="text-color:green;">PRICE:$`+data['Price']+`</h3>
-          <a href="http://localhost:80/wordpress/wordpress/wp-content/plugins/crea/ListingDetail.php?ID=`+data['ID']+`" target="_blank"><h4 class="card-title">`+data['Address']+`</h4></a>
-          <p class="lead mb-2"><strong>`+data['City']+`,`+data['Province']+`</strong></p>
-          <ul class="list-unstyled list-inline d-flex justify-content-between mb-0">
+    //   var html=`
+    //   <div class="col-md-4">
+    // <div class="card" >
+    //     <h1>`+data['TransactionType']+`</h1>
+    //     <h4>ID#`+data['ID']+`</h4>
+    //     <a href="http://localhost:80/wordpress/wordpress/wp-content/plugins/crea/ListingDetail.php?ID=`+data['ID']+`" target="_blank"><img class="card-img-top" src="`+data['Photo']+`" alt="Card image"></a>
+    //     <div class="card-body">
+    //       <h3 style="text-color:green;">PRICE:$`+data['Price']+`</h3>
+    //       <a href="http://localhost:80/wordpress/wordpress/wp-content/plugins/crea/ListingDetail.php?ID=`+data['ID']+`" target="_blank"><h4 class="card-title">`+data['Address']+`</h4></a>
+    //       <p class="lead mb-2"><strong>`+data['City']+`,`+data['Province']+`</strong></p>
+    //       <ul class="list-unstyled list-inline d-flex justify-content-between mb-0">
         
-          <li class="list-inline-item mr-0">
-          <div class="chip mr-0">`+data['Bedroom']+` BedRooms</div>
-          </li>
+    //       <li class="list-inline-item mr-0">
+    //       <div class="chip mr-0">`+data['Bedroom']+` BedRooms</div>
+    //       </li>
 
-          <li class="list-inline-item mr-0">
-          <div class="chip mr-0">`+data['Bathroom']+` BathRooms</div>
-          </li>
-        <li class="list-inline-item mr-0">
-          <div class="chip deep-purple white-text mr-0">`+data['Size']+` Sqft</div>
-          </li>
+    //       <li class="list-inline-item mr-0">
+    //       <div class="chip mr-0">`+data['Bathroom']+` BathRooms</div>
+    //       </li>
+    //     <li class="list-inline-item mr-0">
+    //       <div class="chip deep-purple white-text mr-0">`+data['Size']+` Sqft</div>
+    //       </li>
         
 
-        </ul>
+    //     </ul>
   
           
-        </div>
-        <div class="card-footer"><i>`+data['Listing Office']+`</i></div>
+    //     </div>
+    //     <div class="card-footer"><i>`+data['Listing Office']+`</i></div>
       
-    </div>
-    </div>
-    `;
+    // </div>
+    // </div>
+    // `;
+    var html=` <div class="col-lg-4 ">
+    <!-- Card -->
+<div class="card booking-card">
+
+  <!-- Card image -->
+  <div class="view overlay">
+
+    
+    <a href="http://localhost:80/wordpress/wordpress/wp-content/plugins/crea/ListingDetail.php?ID=`+data['ID']+` target="_blank">
+      <div class="mask rgba-white-slight"><img class="card-img-top" src="`+data['Photo']+`" alt="Card image cap"></div>
+    </a>
+  </div>
+
+  <!-- Card content -->
+  <div class="card-body">
+<div class="ribbon ribbon-top-left"><span>`+data['TransactionType']+`</span></div>
+    <!-- Title -->
+    <h2 class="card-title font-weight-bold"><a>`+data['Price']+`</a></h2>
+    <!-- Data -->
+    <p class="mb-2">`+data['City']+`,`+data['Province']+`</p>
+    <!-- Text -->
+    <p class="card-text">
+    <span>`+data['Bathroom']+` BathRooms</span>
+    <span>`+data['Bedroom']+` BedRooms</span>
+    <span>`+data['Size']+` sqft</span>
+
+    </p>
+    <hr class="my-4">
+    <p class="lead"><strong>`+data['Listing Office']+`</strong></p>
+    <!-- Button -->
+    
+
+  </div>
+
+</div>
+<!-- Card -->
+  </div>`;
 
 
     //alert(html);
