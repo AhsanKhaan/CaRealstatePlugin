6d33de85ef2d72b8d0d47fcff5503f436014a29b
Ahsankhaan <ahsankhan.ubit@gmail.com>
2020-04-21 19:49:10 +0500

    create and apply function for selection

--- js/app.js
+++ js/app.js
@@ -8,8 +8,8 @@ $(document).ready(function(){
 
      $.get( url, function(data) {
   //    console.log( "success:"+data );
-      showdata(data);
-//      alert(data);
+      show_selection(data);
+      
     });
   });//Search Button Ends click
    
@@ -64,6 +64,61 @@ $(document).ready(function(){
     });
   }//dataset function
 
+  function show_selection(dataset){
+    $("#card-stack").empty();
+    dataset.property.forEach(function(data,index){
+     // alert(data["TotalCount"]);
+     console.log(index); 
+     //var Limit=0;
+      //if(Limit<9){
+        var html=` <div class="col-lg-4 ">
+        <!-- Card -->
+    <div class="card booking-card">
+    
+      <!-- Card image -->
+      <div class="view overlay">
+    
+        
+        <a href="http://localhost:80/wordpress/wordpress/wp-content/plugins/crea/ListingDetail.php?ID=`+data['ID']+`" target="_blank">
+          <div class="mask rgba-white-slight"><img class="card-img-top" src="`+data['Photo']+`" alt="Card image cap"></div>
+        </a>
+      </div>
+    
+      <!-- Card content -->
+      <div class="card-body">
+    <div class="ribbon ribbon-top-left"><span>`+data['TransactionType']+`</span></div>
+        <!-- Title -->
+        <h2 class="card-title font-weight-bold"><a>`+data['Price']+`</a></h2>
+        <!-- Data -->
+        <p class="mb-2">`+data['City']+`,`+data['Province']+`</p>
+        <!-- Text -->
+        <p class="card-text">
+        <span>`+data['Bathroom']+` BathRooms</span>
+        <span>`+data['Bedroom']+` BedRooms</span>
+        <span>`+data['Size']+` sqft</span>
+    
+        </p>
+        <hr class="my-4">
+        <p class="lead"><strong>`+data['Listing Office']+`</strong></p>
+        <!-- Button -->
+        
+    
+      </div>
+    
+    </div>
+    <!-- Card -->
+      </div>`;
+    
+    
+        //alert(html);
+         $("#card-stack").append(html);
+       //  Limit++;
+      //}else{
+
+     // }
+      
+    });
+  }//data_set selection function
   function init(){
     //total number of pages can be shown from here
     //uses for displaying total pages
@@ -103,7 +158,7 @@ $(document).ready(function(){
           });
        
     }
-});
+});//twbs Pagination
     });
   }//init ends
   
