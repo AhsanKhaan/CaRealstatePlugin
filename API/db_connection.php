09c818ea019745cae970a721b2131521bdaadffa
AhsanKhaan <ahsankhan.ubit@gmail.com>
2020-08-11 12:15:31 +0500

    changes credentials

--- API/db_connection.php
+++ API/db_connection.php
@@ -2,10 +2,14 @@
 
 
  
+ //$dbhost = "35.208.78.213";
+//  $dbuser = "uach8q5vydzdw";
+//  $dbpass = "wordpress123";
+//  $db = "db8h564zb5cd4y";
  $dbhost = "localhost";
- $dbuser = "uach8q5vydzdw";
- $dbpass = "wordpress123";
- $db = "db8h564zb5cd4y";
+ $dbuser = "root";
+ $dbpass = "";
+ $db = "creaddf";
  $conn = new mysqli($dbhost, $dbuser, $dbpass,$db) or die("Connect failed: %s\n". $conn -> error);
  if ($conn->connect_error) {
     die("Connection failed: " . $conn->connect_error);
